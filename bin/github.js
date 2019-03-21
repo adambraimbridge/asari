ba102823a82ba73ee13158327b71a035710a12d1
Simon Plenderleith <simon@simonplend.co.uk>
2019-03-21 11:46:39 +0000

    Add update-notifier
    When the github command line tool is run, the `update-notifier` library
    will check with the npm registry to see if a new version of this package
    has been published, if it has been then it will display a notification
    prompting the user to upgrade this package.
    
    Note: This only applies when this package has been installed globally
    with `npm` i.e. `npm install --global @financial-times/github`

--- bin/github.js
+++ bin/github.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
+const updateNotifier = require("update-notifier");
+
 require("yargs")
 	.commandDir("../src/commands")
 	.demandCommand()
 	.group(['token', 'json'], 'Global Options:')
 	.strict()
 	.help().argv;
+
+const packageJson = require("../package.json");
+updateNotifier({ pkg: packageJson }).notify();
