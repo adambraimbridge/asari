424ae2dab1f44b2f4b8c5d811d4da8b3146d91ca
Adam Braimbridge <adam.braimbridge@ft.com>
2019-04-04 12:10:56 +0100

    Added pulls/deleteComment  and tests

---
+++ src/commands/pulls/deleteComment.js
@@ -0,0 +1,68 @@
+/**
+ * @see: https://octokit.github.io/rest.js/#api-Pulls-deleteComment
+ * const result = await octokit.pulls.deleteComment({owner, repo, comment_id})
+ * /repos/:owner/:repo/pulls/comments/:comment_id
+ */
+const flow = require("lodash.flow")
+const commonYargs = require("../../../lib/common-yargs")
+const printOutput = require("../../../lib/print-output")
+const authenticatedOctokit = require("../../../lib/octokit")
+
+/**
+ * yargs builder function.
+ *
+ * @param {import('yargs').Yargs} yargs - Instance of yargs
+ */
+const builder = yargs => {
+	const baseOptions = flow([
+		commonYargs.withToken,
+		commonYargs.withJson,
+		commonYargs.withOwner,
+		commonYargs.withRepo,
+	])
+
+	return baseOptions(yargs)
+		.option("comment_id", {
+			describe: "The ID of the comment to be deleted.",
+			type: "integer",
+		})
+}
+
+/**
+ * Return the contents of a pull request body and update a pull request.
+ *
+ * @param {object} argv - argv parsed and filtered by yargs
+ * @param {string} argv.token
+ * @param {string} argv.json
+ * @param {string} argv.owner
+ * @param {string} argv.repo
+ * @param {string} argv.comment_id
+ * @throws {Error} - Throws an error if any required properties are invalid
+ */
+const handler = async ({ token, json, owner, repo, comment_id }) => {
+
+	// Ensure that all required properties have values
+	const requiredProperties = {
+		owner,
+		repo,
+		comment_id,
+	}
+	if (Object.values(requiredProperties).some(property => !property)) {
+		throw new Error(`Please provide all required properties: ${Object.keys(requiredProperties).join(", ")}`)
+	}
+	try {
+		const octokit = await authenticatedOctokit({ personalAccessToken: token })
+		const result = await octokit.pulls.deleteComment(requiredProperties)
+		printOutput({ json, resource: result })
+	}
+	catch (error) {
+		throw new Error(error)
+	}
+}
+
+module.exports = {
+	command: "pulls deleteComment",
+	desc: "Delete a comment on an existing pull request",
+	builder,
+	handler,
+}
