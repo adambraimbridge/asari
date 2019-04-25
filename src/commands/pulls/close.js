456fea264a900b78a72bea89be25ce348f1c1c4b
Adam Braimbridge <adam.braimbridge@ft.com>
2019-04-25 15:40:21 +0100

    Tests for commands/pulls/close are now passing.

--- src/commands/pulls/close.js
+++ src/commands/pulls/close.js
@@ -7,6 +7,7 @@
 const flow = require('lodash.flow')
 
 const commonYargs = require('../../../lib/common-yargs')
+const parseGitHubURL = require('../../../lib/parse-github-url')
 const printOutput = require('../../../lib/print-output')
 const authenticatedOctokit = require('../../../lib/octokit')
 
@@ -24,7 +25,21 @@ const builder = yargs => {
 			describe: 'The URL of the GitHub pull request to close. Pattern: [https://][github.com]/[owner]/[repository?]/pull/[number]',
 		}),
 	])
-	return baseOptions(yargs)
+	return (
+		baseOptions(yargs)
+			/**
+			 * Coerce values from the GitHub URL.
+			 */
+			.middleware(argv => {
+				const githubData = parseGitHubURL(argv.githubUrl)
+
+				debugger
+				argv.scope = githubData.scope
+				argv.pull_number = githubData.id
+				argv.owner = githubData.owner
+				argv.repo = githubData.repo
+			})
+	)
 }
 
 /**
@@ -36,26 +51,26 @@ const builder = yargs => {
  * @param {string} argv.owner
  * @param {string} argv.repo
  * @param {string} argv.number
- * @throws {Error} - Throws an error if any required properties are invalid
  */
-const handler = async ({ token, json, owner, repo, number }) => {
+const handler = async ({ token, json, owner, repo, pull_number }) => {
 	const inputs = {
 		owner,
 		repo,
-		number,
+		pull_number,
 		state: 'closed',
 	}
 	try {
 		const octokit = await authenticatedOctokit({ personalAccessToken: token })
 		const result = await octokit.pulls.update(inputs)
-		printOutput({ json, resource: result })
+		const { html_url, state } = result.data
+		printOutput({ json, resource: { html_url, state } })
 	} catch (error) {
 		printOutput({ json, error })
 	}
 }
 
 module.exports = {
-	command: 'close [options]',
+	command: 'close <github-url>',
 	desc: 'Set the state of an existing pull request to `closed`',
 	builder,
 	handler,
--- test/commands/pulls/close.test.js
+++ test/commands/pulls/close.test.js
@@ -8,35 +8,28 @@ nock.disableNetConnect()
 // Reset any mocked network endpoints
 nock.cleanAll()
 
-jest.spyOn(global.console, 'warn')
-afterEach(() => {
-	jest.clearAllMocks()
-})
-
 /**
  * Common Yargs tests
  */
 const commandGroup = 'pulls'
 const command = 'close'
 const requiredOptions = {
-	owner: 'test',
-	repo: 'test',
-	number: 1,
+	'github-url': 'https://github.com/Test-Owner/Test-Repo/pull/1',
 }
 commonTests.describeYargs(yargsModule, commandGroup, command, requiredOptions)
 
 describe('Octokit', () => {
 	// If this endpoint is not called, nock.isDone() will be false.
 	const successResponse = nock('https://api.github.com')
-		.patch('/repos/test/test/pulls/1')
+		.patch('/repos/Test-Owner/Test-Repo/pulls/1')
 		.reply(200, {})
 
 	test('running the command handler triggers a network request of the GitHub API', async () => {
 		await yargsModule.handler({
 			token: 'test',
-			owner: 'test',
-			repo: 'test',
-			number: 1,
+			owner: 'Test-Owner',
+			repo: 'Test-Repo',
+			pull_number: 1,
 		})
 		expect(successResponse.isDone()).toBe(true)
 	})
